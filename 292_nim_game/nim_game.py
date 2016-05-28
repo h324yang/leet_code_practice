43d1fff46432b8e688b9bd67040978567755a184
Leo Yang <thewaytheman@gmail.com>
2016-05-28 13:24:35 +0800

    modifying the redundant part

--- 292_nim_game/nim_game.py
+++ 292_nim_game/nim_game.py
@@ -1,10 +1,7 @@
 import sys
 class Solution(object):
     def canWinNim(self, n):
-        if not n%4 == 0:
-            return True
-        else:
-            return False
+        return not n%4 == 0
 
 if __name__ == "__main__":
     solution = Solution()
