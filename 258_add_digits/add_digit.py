701a8ecd383636f4ea257eb13febddbc57517b7b
Leo Yang <thewaytheman@gmail.com>
2016-06-11 18:11:42 +0800

    adding solution of 258_add_digit

---
+++ 258_add_digits/add_digit.py
@@ -0,0 +1,14 @@
+import sys
+
+class Solution(object):
+    def addDigits(self, num):
+        if num == 0:
+            return 0
+        tmp = num % 9
+        if tmp == 0:
+            return 9
+        else:
+            return tmp
+
+if __name__ == "__main__":
+    print(Solution().addDigits(int(sys.argv[1])))
