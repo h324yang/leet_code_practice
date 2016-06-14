b2141e634c3bd6c20914e5a39dd845dc5c6e4321
Leo Yang <thewaytheman@gmail.com>
2016-06-15 03:51:38 +0800

    modify the way how to do if-else

--- 258_add_digits/add_digit.py
+++ 258_add_digits/add_digit.py
@@ -4,11 +4,12 @@ class Solution(object):
     def addDigits(self, num):
         if num == 0:
             return 0
-        tmp = num % 9
-        if tmp == 0:
-            return 9
         else:
-            return tmp
+            tmp = num % 9
+            if tmp == 0:
+                return 9
+            else:
+                return tmp
 
 if __name__ == "__main__":
     print(Solution().addDigits(int(sys.argv[1])))
